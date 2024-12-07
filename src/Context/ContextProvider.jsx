eaced90986588605db7612a5c3eb2bdd6ebdcc43
Obijuru <158084677+Obijuru@users.noreply.github.com>
2024-12-07 01:15:36 +0100

    in progress

--- src/Context/ContextProvider.jsx
+++ src/Context/ContextProvider.jsx
@@ -6,16 +6,15 @@ const ContextProvider = ({children}) => {
   const [addCart, setAddCart] = useState([]);
 
 const AddToCart = (item) => {
-  console.log(item);
   
   setAddCart((prevCart) => {
     const matchingProduct = prevCart.find((cartItem) => {
-      cartItem.id === item.id
+      return cartItem.id === item.id
     });
     
     if (matchingProduct) {
       return prevCart.map((cartItem) => {
-        cartItem.id === item.id ? {...cartItem, quantity: cartItem.quantity + 1}
+       return cartItem.id === item.id ? {...cartItem, quantity: cartItem.quantity + 1}
         : cartItem
       })
     } else{
--- src/components/cart/CartFeature.jsx
+++ src/components/cart/CartFeature.jsx
@@ -6,7 +6,6 @@ import { AddContext } from "../../Context/ContextProvider";
 const CartFeature = () => {
 
   const {addCart} = useContext(AddContext)
-    
 
   return (
     <div>
@@ -14,58 +13,63 @@ const CartFeature = () => {
       
       <div className="grid grid-cols-gridcols md:grid-cols-1">
 
-        {addCart.map((cart, index) => {
-          console.log(cart);
-          
-          <div className="flex flex-1 ml-4 mt-4 border-4 p-8" key={index} id={index}>
-          <div className="w-20">
-          <img className="w-24 mt-3" src={cart.image} />
+        {addCart.length === 0 ? (<p>Cart empty</p>)
+        :(
+          addCart.map((cart) => {
+            return (
+              <div className="flex flex-1 ml-4 mt-4 border-4 p-8" key={cart.id} id={cart.id} >
+              <div className="w-20">
+              <img className="w-24 mt-3" src={cart.image} />
+              </div>
+            
+              <div className="ml-5">
+                <div className="font-bold italic">{cart.name}</div>
+                <p className="font-bold text-red-700"> ${(cart.priceCents /100).toFixed(2)}</p>
+                <div className="flex">
+                  <div> Quantity: 2 </div>
+                  <button className="ml-2 text-blue-700">Update</button>
+                  <button className="ml-2 text-blue-700">Delete</button>
+                </div>
+              </div>
+    
+              <div className="italic text-xs font-bold whitespace-nowrap">
+              <div className="ml-8 mb-5">
+                <div className="mb-3">Choose a delivery Option:</div>
+                <div className="flex">
+                  <input type="radio"/>
+                  <div className="ml-3">
+                    <div>Tuesday, June 21</div>
+                    <div>FREE SHIPPING</div>
+                  </div>
+                </div>
+              </div>
+    
+              <div className="ml-8 mb-5">
+                <div className="flex">
+                  <input type="radio"/>
+                  <div className="ml-3">
+                    <div>Tuesday, June 21</div>
+                    <div>FREE SHIPPING</div>
+                  </div>
+                </div>
+              </div>
+    
+              <div className="ml-8">
+                <div className="flex">
+                  <input type="radio"/>
+                  <div className="ml-3">
+                    <div>Tuesday, June 21</div>
+                    <div>FREE SHIPPING</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+            )
+       
+      }))}
         
-          <div className="ml-5">
-            <div className="font-bold italic">{cart.name}</div>
-            <p className="font-bold text-red-700"> ${(cart.priceCents /100).toFixed(2)}</p>
-            <div className="flex">
-              <div> Quantity: 2 </div>
-              <button className="ml-2 text-blue-700">Update</button>
-              <button className="ml-2 text-blue-700">Delete</button>
-            </div>
-          </div>
-
-          <div className="italic text-xs font-bold whitespace-nowrap">
-          <div className="ml-8 mb-5">
-            <div className="mb-3">Choose a delivery Option:</div>
-            <div className="flex">
-              <input type="radio"/>
-              <div className="ml-3">
-                <div>Tuesday, June 21</div>
-                <div>FREE SHIPPING</div>
-              </div>
-            </div>
-          </div>
-
-          <div className="ml-8 mb-5">
-            <div className="flex">
-              <input type="radio"/>
-              <div className="ml-3">
-                <div>Tuesday, June 21</div>
-                <div>FREE SHIPPING</div>
-              </div>
-            </div>
-          </div>
-
-          <div className="ml-8">
-            <div className="flex">
-              <input type="radio"/>
-              <div className="ml-3">
-                <div>Tuesday, June 21</div>
-                <div>FREE SHIPPING</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    })}
+  
  
 
       <CartPayment />
